5a3c398c4a3571b08f6bc6a3754b95000c61261c
Josef Dolezal <pepik.ml@gmail.com>
2017-05-18 21:40:49 +0200

    Add configurable output option

--- Sources/Iconic/main.swift
+++ Sources/Iconic/main.swift
@@ -12,11 +12,33 @@ import IconicKit
 import Stencil
 import StencilSwiftKit
 
-let main = command { (path: Path) in
+let outputOption = Option<Path>("output", "./", flag: "o", description: "The path to the file to generate (Omit to generate to stdout)"
+) { path in
+
+    guard path.isDirectory else {
+        throw ArgumentError.invalidType(value: path.description, type: "directory", argument: nil)
+    }
+
+    return path
+}
+
+let fontArgument = Argument<Path>("FONT FILE", description: "Font to parse.") { path in
+    guard path.isFile else {
+        throw ArgumentError.invalidType(value: path.description, type: "file", argument: nil)
+    }
+
+    return path
+}
+
+let main = command(outputOption, fontArgument) { output, fontPath in
+    let fontOutput = output + Path("FontAwesomeIcon.swift")
+    let catalogOutput = output + Path("FontAwesomeCatalog.html")
+    let catalogFontOutput = output + fontPath.lastComponent
+
     let fontParser = IconsFontFileParser()
     let environment = Environment(iconicTemplatePaths: ["./templates"])
 
-    try fontParser.parseFile(atUrl: path.url)
+    try fontParser.parseFile(atUrl: fontPath.url)
     
     let context: [String: Any] = [
         "icons": fontParser.icons,
@@ -28,8 +50,10 @@ let main = command { (path: Path) in
     let iconsEnum = try environment.renderTemplate(name: "iconic-default.stencil", context: context)
     let catalog = try environment.renderTemplate(name: "catalog.stencil", context: context)
 
-    // print(iconsEnum)
-    print(catalog)
+    // Create icons enum, html catalog and copy font file to output directory
+    try fontOutput.write(iconsEnum)
+    try catalogOutput.write(catalog)
+    try fontPath.copy(catalogFontOutput)
 }
 
 main.run()
