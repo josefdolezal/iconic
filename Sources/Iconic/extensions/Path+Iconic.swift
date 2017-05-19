903b51e8a4629f0ba814141b6eaeb63ecabd0bf5
Josef Dolezal <pepik.ml@gmail.com>
2017-05-19 17:36:08 +0200

    Force output files override

---
+++ Sources/Iconic/extensions/Path+Iconic.swift
@@ -0,0 +1,19 @@
+//
+//  Path+Iconic.swift
+//  Iconic
+//
+//  Created by Josef Dolezal on 19/05/2017.
+//
+//
+
+import PathKit
+
+extension Path {
+    func clearBeforeAction(action: (Path) throws -> Void) throws {
+        if exists {
+            try delete()
+        }
+
+        try action(self)
+    }
+}
--- Sources/Iconic/main.swift
+++ Sources/Iconic/main.swift
@@ -12,6 +12,7 @@ import IconicKit
 import Stencil
 import StencilSwiftKit
 
+// Output folder option
 let outputOption = Option<Path>("output", "./", flag: "o", description: "The path to the file to generate (Omit to generate to stdout)"
 ) { path in
 
@@ -22,13 +23,19 @@ let outputOption = Option<Path>("output", "./", flag: "o", description: "The pat
     return path
 }
 
+// Input font path argument
 let fontArgument = Argument<IconFont>("FONT FILE", description: "Font to parse.")
 
 let main = command(outputOption, fontArgument) { output, font in
+    // Actual Icon enum name
     let enumName = "\(font.fontName)Icon"
+    // Path of original font file
     let fontPath = font.path
+    // Generated enum path
     let fontOutput = output + Path("\(enumName).swift")
+    // Generated html catalog path
     let catalogOutput = output + Path("\(enumName)Catalog.html")
+    // Path of font used in html catalog
     let catalogFontOutput = output + fontPath.lastComponent
 
     let fontParser = IconsFontFileParser()
@@ -46,10 +53,11 @@ let main = command(outputOption, fontArgument) { output, font in
     let iconsEnum = try environment.renderTemplate(name: "iconic-default.stencil", context: context)
     let catalog = try environment.renderTemplate(name: "catalog.stencil", context: context)
 
-    // Create icons enum, html catalog and copy font file to output directory
-    try fontOutput.write(iconsEnum)
-    try catalogOutput.write(catalog)
-    try fontPath.copy(catalogFontOutput)
+    // Create icons enum, html catalog and copy font file to output directory,
+    // if file already exists, it's overwritten
+    try fontOutput.clearBeforeAction { try $0.write(iconsEnum) }
+    try catalogOutput.clearBeforeAction { try $0.write(catalog) }
+    try catalogFontOutput.clearBeforeAction { try fontPath.copy($0) }
 }
 
 main.run()
